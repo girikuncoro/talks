e383aff161da4bcd05ecaefe6dee4b9aa52273ad
Giri Kuncoro <girikuncoro@gmail.com>
2018-07-27 19:43:08 +0530

    Remove init script as it is no longer necessary

--- demo-swagger/swagger-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-swagger init spec \
-  --title "A Todo list application" \
-  --description "From the todo list tutorial on goswagger.io" \
-  --version 1.0.0 \
-  --scheme http \
-  --consumes application/io.goswagger.examples.todo-list.v1+json \
-  --produces application/io.goswagger.examples.todo-list.v1+json
